d0029bb0db8f96ac558cb3c3ee236b3078233f07
luisagcenteno84 <luisagcenteno@gmail.com>
2024-09-08 21:51:50 -0700

    deleting and starting from scratch

--- nutrition_app/nutrition_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-admin.site.register(models.Patient)
-admin.site.register(models.Doctor)
-admin.site.register(models.Measurement)
-admin.site.register(models.Appointment)
-admin.site.register(models.Plan)
